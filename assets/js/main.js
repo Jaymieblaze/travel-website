f4d1d099d9a8838441d73219d99d47770ebe540e
Jaymieblaze <jameseyaiaro@gmail.com>
2025-08-13 15:12:18 +0100

    Add scroll reveal animation for various sections and include ScrollReveal library

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,3 +130,20 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+//================SCROLL REVEAL ANIMATION==================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    reset: true, //Repeats the animation every time you scroll
+})
+
+sr.reveal('.home__container, .testimonial__container, .footer__container')
+sr.reveal('.home__title', { delay: 600 })
+sr.reveal('.home__description', { delay: 900 })
+sr.reveal('home__data .button', { delay: 1200 })
+sr.reveal('.destination__card, .gallery__card', { interval: 100 })
+sr.reveal('.join__data', { origin: 'left' })
+sr.reveal('.join__img', { origin: 'right' })
